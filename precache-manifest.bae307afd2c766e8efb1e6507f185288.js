0a349673726a6f87e55d09cee359603b0ec928b1
Stan tan <stanleyhomies@gmail.com>
2024-06-30 23:58:10 +0800

    Updates

---
+++ precache-manifest.bae307afd2c766e8efb1e6507f185288.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6110bac4b7cb2900f83536d685d697b",
+    "url": "/gotclass/index.html"
+  },
+  {
+    "revision": "a02a2b063966059c5533",
+    "url": "/gotclass/static/css/main.17cb96c5.chunk.css"
+  },
+  {
+    "revision": "22b185d58c97cc637411",
+    "url": "/gotclass/static/js/2.58f4945a.chunk.js"
+  },
+  {
+    "revision": "211c13e79de2da6e3581",
+    "url": "/gotclass/static/js/3.545aa518.chunk.js"
+  },
+  {
+    "revision": "a02a2b063966059c5533",
+    "url": "/gotclass/static/js/main.ca57a61a.chunk.js"
+  },
+  {
+    "revision": "65248c3918d48f9cc29e",
+    "url": "/gotclass/static/js/runtime~main.70387a52.js"
+  },
+  {
+    "revision": "bb2c61ce3af97ea622e0850972aeb8b0",
+    "url": "/gotclass/static/media/clock.bb2c61ce.svg"
+  },
+  {
+    "revision": "c22534167dadd59882e0f12ab84c1bdf",
+    "url": "/gotclass/static/media/klia_bg.c2253416.jpeg"
+  },
+  {
+    "revision": "245bfbdb4f3b5aba6136721f5b533afd",
+    "url": "/gotclass/static/media/map.245bfbdb.svg"
+  },
+  {
+    "revision": "e4ee9a75dc99f91e99f4d868a7343b4a",
+    "url": "/gotclass/static/media/phone.e4ee9a75.svg"
+  },
+  {
+    "revision": "52b14044c17848b5ce86cec3b17d2e08",
+    "url": "/gotclass/static/media/serviceImage1.52b14044.png"
+  },
+  {
+    "revision": "b4bd98ea425f8d4ddbda45ffb0df90d5",
+    "url": "/gotclass/static/media/serviceImage2.b4bd98ea.png"
+  },
+  {
+    "revision": "1b3b0304e630638912c505914f2f8bc5",
+    "url": "/gotclass/static/media/serviceImage3.1b3b0304.png"
+  }
+]);
